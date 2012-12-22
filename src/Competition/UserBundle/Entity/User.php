ce3e3baa1490f8ec3f895baad5851671127c7e6e
Jordi Andújar <jordi.andujar@gmail.com>
2012-12-22 11:27:08 +0100

    Added FOS user bundle with basic settings

---
+++ src/Competition/UserBundle/Entity/User.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: jordi
+ * Date: 22/12/12
+ * Time: 10:31
+ * To change this template use File | Settings | File Templates.
+ */
+class User
+{
+
+}
